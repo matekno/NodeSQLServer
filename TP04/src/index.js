8fad07124638c8c909ad6cc6e1ce138341079f2f
Mati Rapoport <mati.rapoport@gmail.com>
2022-06-06 23:43:41 -0300

    faltan try/catch, logger

--- TP04/src/index.js
+++ TP04/src/index.js
@@ -1,12 +1,36 @@
 import PizzaService from "./services/pizza-service.js";
 import logger from "./utils/logger.js";
 
-const main = async () => {
-    const ps = new PizzaService()
-    let pizzas = await ps.getAll();
-    logger.info(pizzas);
-    console.log(pizzas);
+
+const getAll = async () => {
+    let pizzas = await PizzaService.getAll();
+    pizzas.recordset.map(pizza => {console.log(pizza)}) // no anda con mi logger
 }
 
-main();
+const getById = async () => {
+    let pizza = await PizzaService.getById(1);
+    console.log(pizza.recordset[0]);
+}
 
+const create = async (p) => {
+    let result = await PizzaService.insert(p);
+    console.log(result.rowsAffected);
+}
+
+const update = async (p, id) => {
+    let result = await PizzaService.update(p, id);
+    console.log(result.rowsAffected);
+}
+
+const deleteById = async (id) => {
+    let result = await PizzaService.deleteById(id);
+    console.log(result.rowsAffected);
+}
+
+
+const p ={nombre: "Pizza de Jamon", libreGluten: true, importe: 10, descripcion: "Pizza de jamon con queso"};
+// getAll();   
+// getById();
+// create(p);
+// update(p, 1);
+deleteById(12);
